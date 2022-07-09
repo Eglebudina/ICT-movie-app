02e803cc62945b443e507a3d7cef31e3deb559df
Egle Budinaviciute <eglebudina@gmail.com>
2022-07-09 11:17:47 +0100

    Using react-query to manage the browser-based cache.

--- src/index.js
+++ src/index.js
@@ -4,24 +4,38 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Navigate, Routes} from "react-router-dom";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
-import FavouriteMoviesPage from "./pages/favouriteMoviesPage"; // NEW
+import FavouriteMoviesPage from "./pages/favouriteMoviesPage";
 import MovieReviewPage from "./pages/movieReviewPage";
 import UpcomingMoviesPage from "./pages/upcomingMoviesPage";
+import { QueryClientProvider, QueryClient } from "react-query";
+import { ReactQueryDevtools } from 'react-query/devtools'
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 360000,
+      refetchInterval: 360000, 
+      refetchOnWindowFocus: false
+    },
+  },
+});
 const App = () => {
   return (
-    <BrowserRouter>
-      <SiteHeader />      {/* New Header  */}
-      <Routes>
-        <Route path="/reviews/:id" element={<MovieReviewPage/>} />
-        <Route path="/movies/upcoming" element={<UpcomingMoviesPage/>} />
-        <Route path="/movies/favourites" element={<FavouriteMoviesPage/>}
-        />
-        <Route path="/movies/:id" element={<MoviePage/>} />
-        <Route path="/" element={<HomePage />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <SiteHeader />     
+        <Routes>
+          <Route path="/reviews/:id" element={<MovieReviewPage/>} />
+          <Route path="/movies/upcoming" element={<UpcomingMoviesPage/>} />
+          <Route path="/movies/favourites" element={<FavouriteMoviesPage/>}
+          />
+          <Route path="/movies/:id" element={<MoviePage/>} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes> 
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
 
